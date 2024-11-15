9600b22ce1abb605cbed72e75ac78a03464f9877
Yash-aivid <yash.khambhatta@aividtechvision.com>
2024-11-15 11:43:24 +0530

    remove extra react

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Heart, Mail, Phone, Clock } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
